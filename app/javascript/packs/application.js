c6e946922cd670a75116aaa3a504b4509a4b1fa7
AlanDupre <dupre.alan@gmail.com>
2021-06-25 15:47:27 +0200

    dashboard updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,6 +33,7 @@ import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  console.log("bonjour");
   initFlatpickr();
   initMapbox();
 });
