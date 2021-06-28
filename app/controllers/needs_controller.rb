3b72ce0e97315dc87cd687caa4261ee6cabded50
Adrien MARCEAU <amh.marceau@gmail.com>
2021-06-28 12:19:41 +0200

    Added needs in Create and Show of Events

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,17 +1,19 @@
 class NeedsController < ApplicationController
-
   def show
     @need = Need.find(params[:id])
   end
 
   def new
+    @event = Event.find(params[:event_id])
     @need = Need.new
   end
 
   def create
+    @event = Event.find(params[:event_id])
     @need = Need.new(need_params)
+    @need.event = @event
     if @need.save
-      redirect_to event_path
+      redirect_to event_path(@event)
     else
       render :new
     end
@@ -24,16 +26,16 @@ class NeedsController < ApplicationController
   def update
     @need = Need.find(params[:id])
     @need.update(need_params)
-    redirect_to event_path(@need)
+    redirect_to event_path(@need.event)
   end
 
   def destroy
     @need = Need.find(params[:id])
     @need.destroy
-    redirect_to event_path
+    redirect_to event_path(@need.event)
   end
 
-  private 
+  private
 
   def need_params
     params.require(:need).permit(:category, :name, :quantity)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events
-  resources :needs, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :events do
+    resources :needs, only: [:new, :create]
+  end
+  resources :needs, only: [:show, :edit, :update, :destroy]
   get "/dashboard", to: "dashboards#show"
   get "/dashboard", to: "dashboards#owner"
 end
