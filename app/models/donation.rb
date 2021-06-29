348c7881be0851e929b7813d79811ac60aecabf8
Adrien MARCEAU <amh.marceau@gmail.com>
2021-06-29 15:12:39 +0200

    Donation can't be above Need quantity

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,5 +1,8 @@
 class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :need
-  validates_numericality_of :quantity, less_than: 5, message: "error"
+  validates_numericality_of :quantity, less_than_equal_to: 5, message: "error"
+
+  validates_numericality_of :quantity,
+  less_than_or_equal_to: Proc.new { |d| d.need.quantity}
 end
