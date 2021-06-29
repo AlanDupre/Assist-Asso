cbf8dd869f6cfd104cf9ff53e83d036f1773ba10
Maxence Laplante <maxence.laplante@gmail.com>
2021-06-29 16:45:54 +0200

    dashboard / Progression

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,18 @@ class DashboardsController < ApplicationController
 
   def show
   @user = current_user
-  @events = Event.last(1)
+  @events = @user.events
+  @result = 0.to_f
+  @total = 0.to_f
+  
+    @events.each do |event|
+      event.donations.each do |donation|
+      @result +=  donation.quantity 
+      end
+      event.needs.each do |need|
+        @total += need.quantity
+      end
+    end
+    @percent = (@result/@total * 100).truncate
   end
 end
